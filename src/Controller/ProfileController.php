49090ae8db2b1d6c5b37cf46c64164ecc42f38b8
youngbokz <yanboks@hotmail.com>
2019-10-30 11:05:37 +0100

    Create pages for user profile dashboard. Therefore add new method in ProfileController

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -14,4 +14,28 @@ class ProfileController extends AbstractController
     {
         return $this->render('profile/dashboard.html.twig');
     }
+
+    /**
+     * @Route("/editProfile", name="edit_profile")
+     */
+    public function editProfile()
+    {
+        return $this->render('profile/edit_profile.html.twig');
+    }
+
+    /**
+     * @Route("/userComments", name="user_comments")
+     */
+    public function userComments()
+    {
+        return $this->render('profile/comments.html.twig');
+    }
+
+    /**
+     * @Route("/userReportedCom", name="user_reported_com")
+     */
+    public function userReportedCom()
+    {
+        return $this->render('profile/reported.html.twig');
+    }
 }
