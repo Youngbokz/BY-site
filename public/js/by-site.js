88425fcfabba461f4caabb8c442cacfd98cb43c7
BOKOLO N'DJALI Yannick <yanboks@hotmail.com>
2019-11-09 23:10:09 +0100

    main navbar completed with white background on scroll. Change logo too.

--- public/js/by-site.js
+++ public/js/by-site.js
@@ -7,3 +7,13 @@ function closeMyNav(){
     document.getElementById('my_nav').style.height = "0%";
     document.getElementById('myBurger').style.display = 'block';
 }
+
+//NavBar on scroll
+$(window).on('scroll', function(){
+    if($(window).scrollTop()){
+        $('#mainNav').addClass('black');
+    }
+    else{
+        $('#mainNav').removeClass('black')
+    }
+})
